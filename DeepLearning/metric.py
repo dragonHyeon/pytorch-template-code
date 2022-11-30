b01562277aa97d1f55f88671ef295767666584e6
dragonhyeon <kyh.mv@movements.kr>
2022-11-30 15:21:10 +0900

    11: metric code changed

--- DeepLearning/metric.py
+++ DeepLearning/metric.py
@@ -11,7 +11,7 @@ def accuracy(y_pred, y):
 
     # tensor 를 ndarray 로 변환
     # 배치 내 각각의 결과에 대해 가장 큰 값으로 output 설정 (ex. [0.1, 0.1, 0.8, 0] -> 2)
-    y_pred = np.argmax(a=y_pred.cpu().detach().numpy(),
+    y_pred = np.argmax(a=y_pred.tolist(),
                        axis=1)
     y = y.cpu().numpy()
 
